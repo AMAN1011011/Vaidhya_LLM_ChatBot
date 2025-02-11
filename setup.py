d64b63f688b516dc004e7ae767146c0b8e89b2fd
Aman Verma <95334005+AMAN1011011@users.noreply.github.com>
2025-02-11 19:46:07 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+<<<<<<< HEAD
+    name = 'LLM Project',
+    version= '0.0.0',
+    author= 'Aman Verma',
+    author_email= '10verma2002aman@gmail.com',
+=======
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Bappy Ahmed',
+    author_email= 'entbappy73@gmail.com',
+>>>>>>> f5ce30383767387acec8999bfbc4494d48045fd1
+    packages= find_packages(),
+    install_requires = []
+
+)
